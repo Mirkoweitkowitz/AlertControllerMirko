617b481203ba464ec9041bbd1dd8b75f5be282b4
Mirkoweitkowitz <99251369+Mirkoweitkowitz@users.noreply.github.com>
2022-08-31 09:54:51 +0200

    Initial Commit

---
+++ AlertControllerMirko/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AlertControllerMirko
+//
+//  Created by Mirko Weitkowitz on 31.08.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
